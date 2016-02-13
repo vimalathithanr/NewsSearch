df58b20146f57bb84fc2d8c82cbae3ae9c6b0975
Vimalathithan Rajasekaran <vraja03@safeway.com>
2016-02-12 18:30:50 -0800

    Cleaned the code

--- app/src/main/java/com/example/vraja03/newsearch/activities/ArticleActivity.java
+++ app/src/main/java/com/example/vraja03/newsearch/activities/ArticleActivity.java
@@ -1,11 +1,8 @@
 package com.example.vraja03.newsearch.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
